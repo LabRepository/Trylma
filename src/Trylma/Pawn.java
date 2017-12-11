9d38ac3f11ace2a994673d89f7c6affe2e871e4b
LabRepository <czyszczonikjakub@gmail.com>
2017-12-11 08:22:11 +0100

    Add Basic Pawn implementation

--- src/Trylma/Pawn.java
+++ src/Trylma/Pawn.java
@@ -1,5 +1,26 @@
 package Trylma;
 
 public class Pawn {
-    // TODO
+    Color color;
+    private int x;
+    private int y;
+
+    Pawn(int x, int y, Color color) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+
+    public void move(int x, int y) {
+        if ((x > 0)) {
+            this.x++;
+        } else {
+            this.x--;
+        }
+        if ((y > 0)) {
+            this.y++;
+        } else {
+            this.y--;
+        }
+    }
 }
