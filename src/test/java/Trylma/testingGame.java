8352797126f38c36cb4baa13b806f23dcded5988
Razzerr <michalmb97@hotmail.com>
2017-12-26 20:16:06 +0100

    Corrected and improved algorythm for checking moves, added actual function to change the state of the final movement field

--- src/test/java/Trylma/testingGame.java
+++ src/test/java/Trylma/testingGame.java
@@ -11,7 +11,8 @@ public class testingGame {
         try {
             Game game = new Game(6, 1);
             game.board.board[12][12].setState("BLACKPAWN");
-            assertEquals(true, game.legalmove(13, 11, 11, 13));
+            game.board.board[10][12].setState("BLACKPAWN");
+            assertEquals(true, game.legalmove(12, 12, 8, 12));
         } catch (RuntimeException r) {
             System.out.print(r.getMessage());
         }
